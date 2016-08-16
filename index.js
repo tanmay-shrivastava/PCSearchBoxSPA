b974c179c577328c217e60345819f957c9044850
tanmay-shrivastava <tanmay.shrivastava@gmail.com>
2016-08-16 02:00:06 -0700

    Add files via upload
    Improved version with templating

--- index.js
+++ index.js
@@ -6,29 +6,28 @@ $(function() {
         var MAX_SUGGESTIONS = options.MAX_SUGGESTIONS;
         var products = [];
         var currentMatchesLength = 0;
-        var currentHighlightedIndex = -1;
+        var currentHighlightedIndex = 0;
 
         //Create ul and add it as a sibling to the input box
-        var $suggestionsList = $("<ul/>").addClass("suggestions-list");
-        $searchbox.after($suggestionsList);
+        var suggestionsTemplate = getTemplate('suggestions');
+        var html = suggestionsTemplate(Array(options.MAX_SUGGESTIONS).fill({}));
+        $searchbox.after(html.replace(/[\u200B]/g, ''));
+        var $suggestionsList = $(".suggestions-list");
+        var $suggestionsNomatch = $suggestionsList.find(".suggestions-nomatch");
+        var $suggestionsItems = $suggestionsList.find(".suggestions-item");
 
-        //Create li elements space holders
-        for (var i = 0; i < options.MAX_SUGGESTIONS; i++) {
-            var li = $('<li/>')
-		                .addClass('suggestions-item')
-		                .attr('url', "")
-		                .appendTo($suggestionsList)
-		                .hover(
-		                    function() {
-		                        $(this).addClass("highlighted");
-		                    },
-		                    function() {
-		                        $(this).removeClass("highlighted");
-		                    }
-		                ).click(function(e) {
-		                    navigate($(this).attr("url"));
-		                });
-        }
+        $suggestionsItems
+            .click(function(e) {
+                navigate($(this).attr("url"));
+            })
+            .hover(
+                function(){
+                    $(this).addClass("highlighted");
+                },
+                function(){
+                    $(this).removeClass("highlighted");
+                }
+            );
 
         //Get data for search and cache it
         $.getJSON(url, function(data) {
@@ -42,7 +41,7 @@ $(function() {
 		                .toLowerCase()
 		                .trim();
             if (!val) {
-                $suggestsionsList.hide();
+                $suggestionsList.hide();
                 return;
             }
             var currentMatches = products.filter(function(product, index) {
@@ -50,20 +49,29 @@ $(function() {
                 return found >= 0;
             });
 
-            currentMatchesLength = Math.min(currentMatches.length, MAX_SUGGESTIONS);
+            currentMatchesLength = Math.min(currentMatches.length, options.MAX_SUGGESTIONS);
             if (currentMatchesLength > 0) {
+                $suggestionsNomatch.hide();
                 $suggestionsList.show();
+            } else {
+                $suggestionsNomatch
+                    .show()
+                    .css({display: "block"});;
             }
-            $suggestionsList.find("li").each(function(index) {
+            $suggestionsList.find(".suggestions-item").each(function(index) {
                 if (index < currentMatchesLength) { //make visible and change val
                     var name = currentMatches[index].name;
                     name = name.replace($searchbox.val(), ("<b>" + $searchbox.val() + "</b>"));
                     $(this)
                         .show()
-                        .html("<span class=\"suggestion-text\">" + name + "</span><span class=\"suggestion-category\">" + " in " + currentMatches[index].type + "<span>")
-                        .attr("url", currentMatches[index].url).css({
-                            display: "block"
-                        });
+                        .attr("url", currentMatches[index].url)
+                        .css({display: "block"});
+                    $(this)
+                        .find(".suggestions-text")
+                        .html(name);
+                    $(this)
+                        .find(".suggestions-category")
+                        .html(" in " + currentMatches[index].type);
                 } else {
                     $(this).hide();
                 }
@@ -84,15 +92,16 @@ $(function() {
         });
 
         //Updates the current highlighted index and highlights the item in the list
+        //Adjust boundaries, since 0th element is nomatch string
         var adjustCurrentHighlightedIndex = function(down) {
             if (down) { //down
-                if (currentHighlightedIndex === currentMatchesLength - 1)
-                    currentHighlightedIndex = -1;
+                if (currentHighlightedIndex === currentMatchesLength)
+                    currentHighlightedIndex = 1;
                 else
                     currentHighlightedIndex++;
             } else { //up
-                if (currentHighlightedIndex === -1)
-                    currentHighlightedIndex = currentMatchesLength - 1; //last item
+                if (currentHighlightedIndex === 1)
+                    currentHighlightedIndex = currentMatchesLength; //last item
                 else
                     currentHighlightedIndex--;
             }
@@ -100,13 +109,31 @@ $(function() {
                 .children(".highlighted")
                 .removeClass("highlighted");
             var $highlightedItem = $($suggestionsList.children().get(currentHighlightedIndex)).addClass("highlighted");
-            $searchbox.val($highlightedItem.text());
+            $searchbox.val($highlightedItem.find(".suggestions-text").text());
         }
+    };
 
-        //Navigate using the given url
-        var navigate = function(url) {
-            window.location.href = url;
-        };
+    //Util function to Navigate using the given url
+    var navigate = function(url) {
+        window.location.href = url;
+    };
+
+    //Util function to load handlebar templates using sync ajax, 
+    //can be precompiled for faster performance usingnode handlebars.js module
+    var getTemplate = function(name) {
+        if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+            $.ajax({
+                url : '/templates/' + name + '.html',
+                success : function(data) {
+                    if (Handlebars.templates === undefined) {
+                        Handlebars.templates = {};
+                    }
+                    Handlebars.templates[name] = Handlebars.compile(data);
+                },
+                async : false
+            });
+        }
+        return Handlebars.templates[name];
     };
 
     var $searchbox = $(".main-content input");
